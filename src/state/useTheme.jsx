5d2aebc38d1c0ccb002197f654879b1515e11b32
culi <culitif@tuta.io>
2021-08-16 11:48:11 -0700

    use themes

---
+++ src/state/useTheme.jsx
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const ThemeContext = createContext(undefined);
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const styles = {
+    light: `
+      body {
+        --background: #eeee;
+        --text: #000e;
+
+        background-color: var(--background);
+        color: var(--text);
+      }
+    `,
+    dark: `
+      body { 
+        --background: #000e;
+        --text: #eeee;
+        
+        background-color: var(--background);
+        color: var(--text);
+      }
+    `
+  };
+
+  return <>
+    <ThemeContext.Provider 
+      value={{ theme, setTheme }}
+      style={styles[theme]}
+    >
+      <style>
+        {styles[theme]}
+      </style>
+      {children}
+    </ThemeContext.Provider>
+  </>;
+};
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const useTheme = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return { theme, setTheme };
+};
